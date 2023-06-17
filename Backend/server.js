c1828d6356b18a14c4d3de5f802a882fd42f8481
akshay <akshaymahajan065@gmail.com>
2023-06-18 01:18:40 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,21 @@ import userRouter from './Routes/User-route.js';
 import adminRouter from './Routes/Admin-route.js';
 import movieRouter from './Routes/Movie-route.js';
 import bookingRouter from './Routes/Booking-route.js';
-
+import path from 'path'
 
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
+
+app.use(express.static(path.join(__dirname , '../frontend/build')))
+
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname , '../frontend/build/index.html'));
+})
 
 
 
